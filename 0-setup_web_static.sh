9e6970e2f545749c371b2dbfcebc0268f479a189
ismakr <akroutismail01@gmail.com>
2024-05-04 13:38:03 -0700

    0-0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#sets up the web servers for the deployment of web_static
+apt-get update
+apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Bismillah
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+new_str="location\ =\ \//hbnb_static\ {\n\t\talias\ \/data\/web_static\/current\/\n\t};"
+sudo sed -i "s/location\ \//$new_str/" /etc/nginx/sites-available/default
+sudo service nginx restart
