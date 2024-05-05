5c7b2ed377e9c83ac6baaeb5dc46127be2458563
ismakr <akroutismail01@gmail.com>
2024-05-04 17:57:00 -0700

    1-1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,7 +8,7 @@ def do_pack():
     """generates a .tgz archive from the contents of the web_static"""
     local('sudo mkdir -p versions')
     foltime = datetime.now().strftime('%Y%m%d%H%M%S')
-    fl = "versions/web_static_{foltime}.tgz"
+    fl = f"versions/web_static_{foltime}.tgz"
     com = local(f"sudo tar -cvzf {fl} web_static")
     if com.succeeded:
         return fl
