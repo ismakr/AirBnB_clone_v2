561ba59cb321704f177ca08ef60656fc1376f1bb
ismark <akroutismail01@gmail.com>
2024-05-18 16:04:33 -0700

    0-3

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello():
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(strict_slashes=False)
+    app.run(host='0.0.0.0', port=5000)
