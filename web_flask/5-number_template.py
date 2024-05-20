3e7e627ffcb77309337c71186100ba7958ba02a5
ismark <akroutismail01@gmail.com>
2024-05-20 08:01:25 -0700

    5-4

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -46,7 +46,7 @@ def number(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """route python"""
-    return render_template("templates/5-number.html", nb=f"{n}")
+    return render_template("templates/5-number.html", nb=f"{str(n)}")
 
 
 if __name__ == "__main__":
